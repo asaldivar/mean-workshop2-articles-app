4589b1d8fcfe05efda71c988c3756271fada5858
alexander <ag.saldivar@gmail.com>
2015-09-19 07:24:43 -0700

    configure dynamic app environment

--- server.js
+++ server.js
@@ -4,18 +4,23 @@ var mongoose   = require('mongoose');
 var bodyParser = require('body-parser'); // express is an extremely minimalist framework so we need body-parser to help us handle req.body
 var apiRouter = require('./app/config/routes'); // bring in API routes
 
+// pull correct settings per environment
+var config = require('./app/config/config.js')
+var environmentSettings = config.config();
+
 // configure body-parser so we can work with request.body
 app.use(bodyParser.urlencoded({ extended: true })); // handle urleconded bodies; extended true means in any form (not just key-value pairs)
 app.use(bodyParser.json()); // only parsing json
 
 // connect to DB which is on port 27017
-mongoose.connect('mongodb://localhost:27017/mean-articles');
+mongoose.connect(environmentSettings.db);
 
 // apply router middleware
 // namespace your api
 app.use('/api', apiRouter);
 
-var port = 3000;
+// listen to port as defined or default 3000
+var port = process.env.PORT || 3000;
 
 app.get('/', function(request, response) {
   response.json({message: 'this is json'})
