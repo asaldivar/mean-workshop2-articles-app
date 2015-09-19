9197d29e0f3bd57d67bc99332e4123dec3de329f
alexander <ag.saldivar@gmail.com>
2015-09-19 07:11:33 -0700

    DRY :article_id endpoint by using .param middleware

--- app/config/routes.js
+++ app/config/routes.js
@@ -3,6 +3,10 @@ var apiRouter = express.Router();
 var Article = require('../models/article');
 var articlesController = require('../controllers/articles'); // bring in controllers
 
+// param middleware is called before .use() middleware
+// use param to refactor findById code
+apiRouter.param('article_id', articlesController.articleById);
+
 // configure router middleware
 apiRouter.route('/articles')
 
--- app/controllers/articles.js
+++ app/controllers/articles.js
@@ -1,5 +1,14 @@
 var Article = require('../models/article');
 
+function articleById(request, response, next, id) {
+  Article.findById(id, function(error, article) {
+    if (error) console.error('Could not update article b/c:', error);
+
+    request.article = article; // store article in request
+    next(); // callback to move onto next handler
+  });
+}
+
 function create(request, response) {
   // create a new article based of user data
   var article = new Article(request.body);
@@ -13,7 +22,6 @@ function create(request, response) {
 }
 
 function index(request, response) {
-  // get all articles
   Article.find(function(error, articles) {
     if (error) console.error('Could not retrieve articles b/c:', error);
 
@@ -22,42 +30,26 @@ function index(request, response) {
 }
 
 function show(request, response) {
-  //get one article
-  var id = request.params.article_id;
-
-  Article.findById(id, function(error, article) {
-    if (error) console.error('Could not update article b/c:', error);
-
-    response.json(article);
-  });
+  response.json(request.article);
 }
 
 function update(request, response) {
-  // update an article
-  var id = request.params.article_id;
+  var data = request.body;
+  var article = request.article
 
-  Article.findById(id, function(error, article) {
+  Object.keys(data).forEach(function(key) {
+    article.set(key, data[key]); // set replaces the value of a field with the specified value
+  });
+
+  article.save(function(error) {
     if (error) console.error('Could not update article b/c:', error);
 
-      var data = request.body;
-
-      Object.keys(data).forEach(function(key) {
-        article.set(key, data[key]); // set replaces the value of a field with the specified value
-      });
-
-      article.save(function(error) {
-        if (error) console.error('Could not update article b/c:', error);
-
-        response.json({message: 'Article successfully updated'});
-      });
+    response.json({message: 'Article successfully updated'});
   });
 }
 
 function destroy(request, response) {
-  // delete an article
-  var id = request.params.article_id;
-
-  Article.remove({ _id: id }, function(error) {
+  Article.remove({ _id: request.params.article_id }, function(error) {
     if (error) console.error('Could not delete article b/c:', error);
 
     response.json({message: 'Article successfully deleted'});
@@ -65,6 +57,7 @@ function destroy(request, response) {
 }
 
 module.exports = {
+  articleById: articleById,
   create: create,
   index: index,
   show: show,
