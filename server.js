fc0bb9d984a5862132c2a9fd65aebfc16a266333
alexander <ag.saldivar@gmail.com>
2015-09-19 06:49:02 -0700

    install and configure body-parser middleware, bring model into server and give post endpoint controller logic

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ var express = require('express');
 var app = express();
 var apiRouter = express.Router();
 var mongoose   = require('mongoose');
+var Article = require('./app/models/article');
+var bodyParser = require('body-parser'); // express is an extremely minimalist framework so we need body-parser to help us handle req.body
+
+// configure body-parser so we can work with request.body
+app.use(bodyParser.urlencoded({ extended: true })); // handle urleconded bodies; extended true means in any form (not just key-value pairs)
+app.use(bodyParser.json()); // only parsing json
 
 // connect to DB which is on port 27017
 mongoose.connect('mongodb://localhost:27017/mean-articles');
@@ -9,25 +15,33 @@ mongoose.connect('mongodb://localhost:27017/mean-articles');
 // configure router middleware
 apiRouter.route('/articles')
 
-  .post(function() {
-    // create data
+  .post(function(request, response) {
+    // create a new article based of user data
+    var article = new Article(request.body);
+
+    // save the article
+    article.save(function(error) {
+      if (error) console.error('Not able to create article b/c:', error);
+
+      response.json({message: 'Article successfully created'});
+    });
   })
 
-  .get(function() {
+  .get(function(request, response) {
     // get all articles
   });
 
 apiRouter.route('/article/:article_id')
 
-  .get(function() {
+  .get(function(request, response) {
     //get one article
   })
 
-  .patch(function() {
+  .patch(function(request, response) {
     // update an article
   })
 
-  .delete(function() {
+  .delete(function(request, response) {
     // delete an article
   });
 
